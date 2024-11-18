7c65a47a1b8430478d7164942b83c94f085d2c4d
Youssef Medaghri-Alaoui <ymedaghri@gmail.com>
2024-11-18 18:09:37 +0100

    Episode 1 : Next JS

---
+++ app/(my-layout)/repositories/DeleteRepositoryButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react'
+
+interface DeleteRepositoryButtonProps {
+    repoName: string;
+    deleteAction: (repoName:string) => void;
+  }
+  
+const DeleteRepositoryButton = ({repoName, deleteAction}:DeleteRepositoryButtonProps) => {
+
+async function handleDelete() {
+    const confirmed = confirm(`Are you sure you want to delete ${repoName} ?`);
+    if (!confirmed) return;
+
+    await deleteAction(repoName);
+  }
+
+  return (
+    <button className="my-destroy-button" onClick={handleDelete}>
+        Delete
+    </button>
+  )
+}
+
+export default DeleteRepositoryButton
---
+++ app/(my-layout)/repositories/page.tsx
@@ -0,0 +1,71 @@
+import Link from 'next/link'
+import React from 'react'
+import DeleteRepositoryButton from './DeleteRepositoryButton';
+import { revalidatePath } from 'next/cache';
+import { getDb } from '@/lib/db';
+
+interface Repository {
+  name: string;
+  description:string;
+  clone_url:string;
+ }
+
+ async function deleteRepository(repoName: string) {
+  "use server";
+  const db = await getDb(); 
+  db.data.repositories = db.data.repositories.filter((repository) => repository.name !== repoName);
+  await db.write();
+
+  revalidatePath('/repositories');
+}
+
+
+const RepositoriesPage = async () => {
+
+  const res = await fetch('http://localhost:3000/api/repositories', {cache: 'no-store'});
+  const data: Repository[] = await res.json();
+   
+
+  return (
+    <>   
+      <div className="my-grid-panel">
+        <h2 className="my-standard-text">Repositories</h2>
+        <Link className="my-primary-button" href={"/repositories/new"}>
+          Add New
+        </Link>
+      </div>
+      
+      <div className="my-regular-table">
+        <div>    
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Uri</th>
+              <th className='w-52'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((element, index) => (
+              <tr key={`repository_${index}`}>
+              <td>{element.name}</td>
+              <td>{element.description}</td>
+              <td>{element.clone_url}</td>
+              <td className="space-x-2">
+                <button className="my-action-button">
+                  Edit
+                </button>
+                <DeleteRepositoryButton repoName={element.name} deleteAction={deleteRepository}/>
+              </td>
+            </tr>
+            ))}            
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default RepositoriesPage
